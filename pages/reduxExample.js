f35af20571cc1db487ee9393c53e1c151a9a78f3
HwangSungIn <gommpo@yanolja.com>
2018-04-27 20:18:59 +0900

    #작업내역
    ducks 구조와 일부 redux 에 대한 구조화 작업
    connect 에 대한 설정 및 store의 초기화 시점을 최초 로드 한번으로 하고 싶은데 Next에서의 방법을 좀 찾아봐야 할 듯 하다.

--- pages/reduxExample.js
+++ pages/reduxExample.js
@@ -1,37 +1,42 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { initStore, startClock, addCount, serverRenderClock } from '../store/configure';
+import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux'
+import { increment, decrement } from '../store/modules/reduxExampleReducer';
 import withRedux from '../util/withRedux';
+import {initStore} from "../store/configure";
 
 class reduxExample extends React.Component{
-    static getInitialProps ({ store, isServer }) {
-        store.dispatch(serverRenderClock(isServer));
-        store.dispatch(addCount());
 
-        return { isServer }
-    }
 
     componentDidMount () {
-        this.timer = this.props.startClock()
     }
 
     componentWillUnmount () {
-        clearInterval(this.timer)
     }
 
     render () {
+        console.log(this.props);
         return (
             <div>
                 <span>redux-tesign</span>
+                <br/>
+                <span>count : <b>{this.props.number}</b></span>
+                <br/>
+                <button onClick={()=>{this.props.increment()}}>number Increase</button>
+                <button onClick={()=>{this.props.decrement()}}>number Decrease</button>
+
             </div>
         )
     }
 }
 
+const mapStateToProps = ({ number }) => ({ number });
+
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        addCount: bindActionCreators(addCount, dispatch),
-        startClock: bindActionCreators(startClock, dispatch)
+        increment: bindActionCreators(increment, dispatch),
+        decrement: bindActionCreators(decrement, dispatch)
     }
 };
-export default withRedux(initStore, null, mapDispatchToProps)(reduxExample)
+export default (withRedux(initStore, mapStateToProps, mapDispatchToProps)(reduxExample))
---
+++ store/modules/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import reduxExampleReducer from './reduxExampleReducer';
+
+export default combineReducers({
+    reduxExampleReducer
+});
